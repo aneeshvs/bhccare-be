ec067eafd3831aecbf025f952dae9b07c232382e
Evan <evantabraham842@gmail.com>
2025-09-19 11:03:49 +0530

    small change

--- app/Models/SupportCarePlanCommunicationPlan.php
+++ app/Models/SupportCarePlanCommunicationPlan.php
@@ -9,6 +9,7 @@ use Spatie\Activitylog\LogOptions;
 class SupportCarePlanCommunicationPlan extends DefaultDBModel
 {
     use LogsActivity;
+            protected $table  = 'communication_plans';
 
     protected $fillable = [
         'support_care_plan_id',
